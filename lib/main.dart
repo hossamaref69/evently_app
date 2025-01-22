c3eec27c35833fc42e9d44d532a04181ef48eba9
Hossam aref <hossamaref62@gmail.com>
2025-01-22 04:58:32 +0200

    Setup Firebase and add project configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
